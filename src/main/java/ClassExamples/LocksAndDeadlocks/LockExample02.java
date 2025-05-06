0854150c16a5755f4ac0570bf8c05811f7b07352
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-06 20:22:51 +0330

    feat: complete LockExample02

--- src/main/java/ClassExamples/LocksAndDeadlocks/LockExample02.java
+++ src/main/java/ClassExamples/LocksAndDeadlocks/LockExample02.java
@@ -5,24 +5,22 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class LockExample02 {
 
+        static Lock lock = new ReentrantLock();
         static class MyRunnable implements Runnable {
             private final int data;
-            private final Lock lock;
 
-            public MyRunnable(int data, Lock lock) {
+            public MyRunnable(int data) {
                 this.data = data;
-                this.lock = lock;
             }
 
             public void run() {
-                lock.lock();
                 LockExample02.operation(data);
-                lock.unlock();
             }
         }
 
         static void operation(int data) {
 
+            lock.lock();
             for(int i = 1; i <= data; i++) {
                 for(int j = 1; j <= i; j++) {
 
@@ -31,13 +29,12 @@ public class LockExample02 {
                 System.out.println();
             }
             System.out.println();
+            lock.unlock();
         }
         public static void main(String[] args) {
-            Lock lock = new ReentrantLock();
-
-            Thread thread1 = new Thread(new MyRunnable(3, lock));
-            Thread thread2 = new Thread(new MyRunnable(5, lock));
-            Thread thread3 = new Thread(new MyRunnable(7, lock));
+            Thread thread1 = new Thread(new MyRunnable(3));
+            Thread thread2 = new Thread(new MyRunnable(5));
+            Thread thread3 = new Thread(new MyRunnable(7));
 
             thread1.start();
             thread2.start();
