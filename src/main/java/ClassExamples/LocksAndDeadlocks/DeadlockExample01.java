e6ebd39d281d1416a30729b7f0f1cc70345699a3
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-06 13:37:29 +0330

    feat: add DeadlockExample01

---
+++ src/main/java/ClassExamples/LocksAndDeadlocks/DeadlockExample01.java
@@ -0,0 +1,44 @@
+package ClassExamples.LocksAndDeadlocks;
+
+public class DeadlockExample01 {
+        static class Resource {
+            private final int id;
+            private final String name;
+
+            public Resource(int id, String name) {
+                this.id = id;
+                this.name = name;
+            }
+
+            public int getId() {
+                return id;
+            }
+
+            public String getName() {
+                return name;
+            }
+        }
+        public static void useResources(Resource r1, Resource r2) {
+            System.out.println(Thread.currentThread().getName() + " is attempting to lock " + r1.getName());
+            synchronized (r1) {
+                System.out.println("+ " +Thread.currentThread().getName() + " locked " + r1.getName());
+                try { Thread.sleep(100); } catch (InterruptedException ignored) {}
+                System.out.println(Thread.currentThread().getName() + " is attempting to lock " + r2.getName());
+                synchronized (r2) {
+                    System.out.println("+ " +Thread.currentThread().getName() + " locked " + r2.getName());
+                    System.out.println(Thread.currentThread().getName() + " using " + r1.getName() + " and " + r2.getName());
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            Resource resA = new Resource(1, "ResourceA");
+            Resource resB = new Resource(2, "ResourceB");
+
+            Thread t1 = new Thread(() -> useResources(resA, resB), "Thread 1");
+            Thread t2 = new Thread(() -> useResources(resB, resA), "Thread 2"); // opposite order → possible deadlock
+
+            t1.start();
+            t2.start();
+        }
+    }
